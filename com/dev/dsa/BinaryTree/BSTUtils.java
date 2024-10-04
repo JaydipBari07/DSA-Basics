eb10a3c9974d37b6df460a6243dc5321ec1d506e
jaba0719 <jaydip.bari@netcracker.com>
2024-10-04 12:01:01 +0530

    Comments added in BST implementation

--- com/dev/dsa/BinaryTree/BSTUtils.java
+++ com/dev/dsa/BinaryTree/BSTUtils.java
@@ -6,6 +6,11 @@ import static com.dev.dsa.BinaryTree.SearchInBST.searchBST;
 
 public class BSTUtils {
 
+    /**
+     * Creates BST from provided array
+     * @param arr Array from which tree is to be created
+     * @return Root node of the created tree
+     */
     public static TreeNode createBst(int[] arr) {
         TreeNode root = new TreeNode(arr[0]);
         for (int i = 1; i < arr.length; i++) {
@@ -14,6 +19,11 @@ public class BSTUtils {
         return root;
     }
 
+    /**
+     * Insert a node into BST
+     * @param root Root node of BST
+     * @param val Value to be inserted
+     */
     public static void insertElement(TreeNode root, int val) {
         if (root.val > val) {
             if (root.left != null) {
@@ -30,6 +40,10 @@ public class BSTUtils {
         }
     }
 
+    /**
+     * Traverses BST using InOrder traversal to give ascending sorted array
+     * @param root Root node of the tree
+     */
     public static void printBSTInAscendingOrder(TreeNode root) {
         if (root != null) {
             printBSTInAscendingOrder(root.left);
@@ -38,6 +52,12 @@ public class BSTUtils {
         }
     }
 
+    /**
+     * Deletes the node from BST
+     * @param root Root node of the tree
+     * @param val Value to be deleted from BST
+     * @return Returns root node of updated tree
+     */
     public static TreeNode deleteNode(TreeNode root, int val) {
         if (root == null) {
             return null;
@@ -60,6 +80,11 @@ public class BSTUtils {
         return root;
     }
 
+    /**
+     * Finds Minimum valued node in the tree
+     * @param root  Root Node of the tree
+     * @return Returns minimum node from the tree
+     */
     public static TreeNode findMin(TreeNode root) {
         if (root == null) {
             return null;
